3f57bdb76d61847ad44a2ada1b6e8d1423a2089a
jhanker <mohammadshahin460@gmail.com>
2020-07-19 14:48:48 +0300

    second commit

--- feetToMile.js
+++ feetToMile.js
@@ -4,3 +4,20 @@ function feetToMile(feet) {
 }
 var result = feetToMile(63360);
 console.log(result);
+function woodCalculator(cubicfeet) {
+    var feet = cubicfeet / 0.62995;
+    return feet;
+}
+var chair = woodCalculator(10);
+console.log(chair);
+var table = woodCalculator(15);
+console.log(table);
+var bed = woodCalculator(25);
+console.log(bed);
+ var brickCalculator 
+    var eachFeet = 1000;
+    var for15feet = 15;
+    var total = eachFeet * for15feet;
+
+
+console.log(total);
